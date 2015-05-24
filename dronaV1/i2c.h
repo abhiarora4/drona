dac21c23391e3dff2898f21170ae50627f35093e
Abhishek Arora <abhiarora4@users.noreply.github.com>
2015-05-24 21:06:44 +0530

    Create i2c.h

---
+++ dronaV1/i2c.h
@@ -0,0 +1,55 @@
+const uint8_t IMUAddress = 0x68; // AD0 is logic low on the PCB
+const uint16_t I2C_TIMEOUT = 1000; // Used to check for errors in I2C communication
+
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop) {
+  return i2cWrite(registerAddress, &data, 1, sendStop); // Returns 0 on success
+}
+
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop) {
+  Wire.beginTransmission(IMUAddress);
+  Wire.write(registerAddress);
+  Wire.write(data, length);
+  uint8_t rcode = Wire.endTransmission(sendStop); // Returns 0 on success
+  if (rcode) {
+    motorArm();
+    terminal.println("imu disconnected");
+    //terminal.print(0);
+    imu = false;
+    //terminal.println(rcode);
+  }
+  return rcode; 
+}
+
+uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
+  uint32_t timeOutTimer;
+  Wire.beginTransmission(IMUAddress);
+  Wire.write(registerAddress);
+  uint8_t rcode = Wire.endTransmission(false); // Don't release the bus
+  if (rcode) {
+    motorArm();
+    terminal.println("imu disconnected");
+    //terminal.println(0);
+    imu=false;
+    //terminal.println(rcode);
+    return rcode; 
+  }
+  Wire.requestFrom(IMUAddress, nbytes, (uint8_t)true); // Send a repeated start and then release the bus after reading
+  for (uint8_t i = 0; i < nbytes; i++) {
+    if (Wire.available())
+      data[i] = Wire.read();
+    else {
+      timeOutTimer = micros();
+      while (((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
+      if (Wire.available())
+        data[i] = Wire.read();
+      else {
+        motorArm();
+        terminal.println("imu disconnected");
+        imu=false;
+        //terminal.println(0);
+        return 5; 
+      }
+    }
+  }
+  return 0; 
+}
